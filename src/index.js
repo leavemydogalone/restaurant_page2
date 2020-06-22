75806b8b9ed78103de052b55f887569ef7ae75be
Leavemydogalone <niklasport88@gmail.com>
2020-06-22 11:49:08 -0700

    added stuff

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import _ from 'lodash';
+const div = document.querySelector('#content');
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+const menuPage = (()=> {
+    let headDiv = document.createElement('div');
+    let mainDiv = document.createElement('div');
+    div.appendChild(headDiv);
+    div.appendChild(mainDiv);
+    headDiv.classList.add('head');
+    headDiv.textContent = 'Yummy Yummy Tasty Favorite BBQ Restaurant';
+    headDiv.appendChild(document.createElement('p')).textContent = 'Phone: 555-5555  Email: YYTFBBQR';
+    mainDiv.classList.add('main');
+    mainDiv.textContent = 'here is the stuff';
+})();
 
 console.log('dog');
